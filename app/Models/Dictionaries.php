1489db3f09ff9618b1cfc6d39f290467940b26ff
crsmok <museum.grodno@gmail.com>
2021-12-01 17:45:24 +0200

    Перечень справочников. Модель

---
+++ app/Models/Dictionaries.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+class Dictionaries extends Model
+{
+    use HasFactory;
+
+    /**
+     * Таблица БД, ассоциированная с моделью.
+     *
+     * @var string
+     */
+
+    protected $table='dictionaries';
+
+    /**
+     * Первичный ключ таблицы БД.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'dict_id';
+
+
+}
